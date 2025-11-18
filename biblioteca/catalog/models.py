5d3ac6f755155f8f5fbaad18f4efd20936a722eb
diegoequisde <diegoequisde@gmail.com>
2025-11-18 12:49:49 +0100

    modelo Libro funcionando

--- biblioteca/catalog/models.py
+++ biblioteca/catalog/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 class Libro(models.Model):
     titulo = models.CharField(max_length=200)
-    autor = models.CharField(max_length=100)
-    publicado_en = models.DateField()
+    autor_nombre = models.CharField("Nombre del autor", max_length=100)
+    autor_apellidos = models.CharField("Apellidos del autor", max_length=150)
+    genero = models.CharField(max_length=100)
+    paginas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.titulo} ({self.autor_nombre} {self.autor_apellidos})"
